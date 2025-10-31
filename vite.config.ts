efb5ea0921cc62b8a81b999fe7eb3a8cb805d67b
Apple Ma, Jessica Yan <applwaae@gmail.com>
2025-10-31 13:41:33 +0800

    final

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
     }
-  },
-  base: './' // This is crucial for Cloudflare Pages deployment
+  }
 })
