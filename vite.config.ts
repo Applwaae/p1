c3e6336bc6ddf7c12cf9713040a1b6fb2843ecea
Apple Ma, Jessica Yan <applwaae@gmail.com>
2025-10-31 13:15:51 +0800

    fix: update TypeScript configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-
-  base: '/', 
-
-  server: {
-    host: '127.0.0.1'
-  }
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  base: './' // This is crucial for Cloudflare Pages deployment
 })
